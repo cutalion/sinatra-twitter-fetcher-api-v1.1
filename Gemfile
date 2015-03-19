d4695915ac4dab5d0bf1dbf6eef9fdbe138d4cea
Alexander Glushkov <cutalion@gmail.com>
2015-03-19 03:15:46 +0300

    Allow cross origin requests

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 ruby '2.2.0'
 
 gem 'sinatra'
+gem 'sinatra-cross_origin'
 gem 'json'
 gem 'twitter'
 gem 'shotgun'
--- twitter_fetcher.rb
+++ twitter_fetcher.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
 require 'json'
 require 'rubygems'
 require 'twitter'
 
 class TwitterFetcher < Sinatra::Base
+  register Sinatra::CrossOrigin
+
+  configure do
+    enable :cross_origin
+  end
+
   @@twitter_client = Twitter::Client.new(
     :consumer_key       => ENV['TWITTER_CONSUMER_KEY'],
     :consumer_secret    => ENV['TWITTER_CONSUMER_SECRET'],
